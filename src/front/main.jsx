71e0201a625ee46e1bdc102457ca2d92a6ac90ae
Viviana Feijoo <vivfeijoo@gmail.com>
2025-07-15 17:48:17 +0000

    main

--- src/front/main.jsx
+++ src/front/main.jsx
@@ -1,29 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'  // Global styles for your application
-import { RouterProvider } from "react-router-dom";  // Import RouterProvider to use the router
-import AppRoutes from "./front/routes.jsx";
-import { StoreProvider } from './hooks/useGlobalReducer';  // Import the StoreProvider for global state management
-import { BackendURL } from './components/BackendURL';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../styles/index.css";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes.jsx";
+import { StoreProvider } from "./hooks/useGlobalReducer.jsx";
+import BackendURL from "./components/BackendURL.jsx";
 
-const Main = () => {
-    
-    if(! import.meta.env.VITE_BACKEND_URL ||  import.meta.env.VITE_BACKEND_URL == "") return (
-        <React.StrictMode>
-              <BackendURL/ >
-        </React.StrictMode>
-        );
-    return (
-        <React.StrictMode>  
-            {/* Provide global state to all components */}
-            <StoreProvider> 
-                {/* Set up routing for the application */} 
-                <RouterProvider router={router}>
-                </RouterProvider>
+ReactDOM.createRoot(document.getElementById("app")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <StoreProvider>
+                <BackendURL>
+                    <AppRoutes />
+                </BackendURL>
             </StoreProvider>
-        </React.StrictMode>
-    );
-}
+        </BrowserRouter>
+    </React.StrictMode>
+);
 
-// Render the Main component into the root DOM element.
-ReactDOM.createRoot(document.getElementById('root')).render(<Main />)
